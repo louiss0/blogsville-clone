fc09f1365a67d49e1dbc84161a3a207f1bed90fb
Shelton Louis <louisshelton0@gmail.com>
2022-09-16 18:43:19 -0400

    finished the membership page

--- app/src/components/FAQAccordion.vue
+++ app/src/components/FAQAccordion.vue
@@ -1,38 +1,46 @@
 <script lang="ts" setup>
 import { useToggle } from "@vueuse/shared";
+import { reactive } from "vue";
 
-const [accordionIsShown, toggleAccordion] = useToggle();
+const [accordionIsShown, toggleAccordion] = useToggle(false);
 
 defineProps<{
   question: string;
   answer: string;
 }>();
+
+const reactiveClasses = reactive({
+  get classesUsedToRaiseAnElementsHeight() {
+    return [
+      "transition-all  duration-300 ease-in overflow-hidden",
+      !accordionIsShown.value ? "h-0" : "h-full",
+    ];
+  },
+  get classesUsedToSmoothlyRotateAnElement() {
+    return [
+      "transition-transform duration-300 ease-in delay-200",
+      { "rotate-180": accordionIsShown.value },
+    ];
+  },
+});
 </script>
 
 <template>
-  <div class="border border-solid bg-gray-300">
-    <div class="bg-gray-200 px-4 py-6">
+  <div class="border border-solid border-gray-300">
+    <div class="px-4 py-6">
       <div class="flex justify-between items-center">
-        <span>{{ question }}</span>
-        <div
-          class="transition-transform duration-500 ease-in"
-          :class="{ 'rotate-y-360': accordionIsShown }"
-        >
+        <span class="font-bold text-lg">{{ question }}</span>
+        <div :class="reactiveClasses.classesUsedToSmoothlyRotateAnElement">
           <button class="text-gray-400" @click="toggleAccordion()">
-            >
-            <slot name="icon-slot"> </slot>
+            <slot name="icon-slot"></slot>
           </button>
         </div>
       </div>
     </div>
-    <div :class="[accordionIsShown ? 'h-0' : 'min-h-fit']">
-      <div
-        class="transition-transform duration-300 ease-in"
-        :class="[accordionIsShown ? 'scale-x-0' : 'scale-x-100']"
-      >
-        <div class="px-4 py-6">
-          <p>{{ answer }}</p>
-        </div>
+
+    <div :class="reactiveClasses.classesUsedToRaiseAnElementsHeight">
+      <div class="px-4 py-6">
+        <p>{{ answer }}</p>
       </div>
     </div>
   </div>
